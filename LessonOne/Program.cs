afdb8cdcaacefd85642dd3f8b50e1ba5e7eb939b
JippyWippy <ilyaseelev76@gmail.com>
2023-03-07 21:08:00 +0700

    Создал консольную программу для вывода введённого пользователем имени с применением интерполяции (Домашка №1)

--- LessonOne/Program.cs
+++ LessonOne/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonOne { 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write("Доброго дня! Введите своё имя: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine($"Привет, {name}!");
+        }
+    }
+}
