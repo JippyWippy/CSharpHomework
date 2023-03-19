70c7d56129e50b0e0b060cb85062479c4bf1869f
JippyWippy <ilyaseelev76@gmail.com>
2023-03-19 20:59:08 +0700

    Домашняя работа №2: создание ручного ввода с определением второго максимального значения. Добавлены исключения

---
+++ LessonTwo/LessonTwo/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                try
+                {
+                    int arrayElement;
+                    Console.Write("Введите размер Вашего массива: ");
+                    int arraySize;
+                    arraySize = Convert.ToInt32(Console.ReadLine());
+
+                    int[] array = new int[arraySize];
+
+                    Console.SetCursorPosition(0, 5);
+
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        Console.Write($"Введите элемент {i + 1} Вашего массива: ");
+
+                        arrayElement = Convert.ToInt32(Console.ReadLine());
+                        array[i] = arrayElement;
+                    }
+
+                    int maxElement = 0;
+                    int prevMaxElement = 0;
+
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        int currentElement = array[i];
+
+                        if (currentElement > maxElement)
+                        {
+                            prevMaxElement = maxElement;
+                            maxElement = currentElement;
+
+                        }
+                        else if (currentElement < maxElement && currentElement > prevMaxElement)
+                        {
+                            prevMaxElement = currentElement;
+                        }
+                    }
+                    Console.WriteLine("Второй наибольший элемент массива: " + prevMaxElement);
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine("Вы ввели неправильное значение для размера массива! Попробуйте ещё раз, нажав любую кнопку!");
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Вы ввели нечисловое значение! Пожалуйста, вводите только числовые значений! Нажммите любую кнопку, чтобы попробовать ещё раз");
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
+                }
+
+            }
+
+        }
+    }
+}
